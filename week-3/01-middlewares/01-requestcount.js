fdceff3869982ee6ad53b6c18d27167c84e7f4c8
Yuvi <bhullar.yuvraj30@gmail.com>
2024-07-16 20:05:38 +0530

    Week 3 01 done

--- week-3/01-middlewares/01-requestcount.js
+++ week-3/01-middlewares/01-requestcount.js
@@ -1,14 +1,12 @@
-const request = require('supertest');
-const assert = require('assert');
 const express = require('express');
 
 const app = express();
 let requestCount = 0;
 
-// You have been given an express server which has a few endpoints.
-// Your task is to create a global middleware (app.use) which will
-// maintain a count of the number of requests made to the server in the global
-// requestCount variable
+app.use(function(req, res, next) {
+  requestCount = requestCount + 1;
+  next();
+});
 
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
@@ -22,4 +20,8 @@ app.get('/requestCount', function(req, res) {
   res.status(200).json({ requestCount });
 });
 
-module.exports = app;
+// listen for requests :) at port 3000
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
